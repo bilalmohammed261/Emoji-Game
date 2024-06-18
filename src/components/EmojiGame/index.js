81556880dcb96964f24b5689f2ccd86c796a2de7
root <bilalmohammed261@gmail.com>
2024-06-18 03:49:48 +0000

    started coding the app

---
+++ src/components/EmojiGame/index.js
@@ -0,0 +1,24 @@
+/* 
+Quick Tip 
+
+- Use the below function in the EmojiGame Component to shuffle the emojisList every time when an emoji is clicked.
+
+const shuffledEmojisList = () => {
+  const {emojisList} = this.props
+  return emojisList.sort(() => Math.random() - 0.5)
+}
+
+*/
+
+// Write your code here.
+import {Component} from 'react'
+
+import NavBar from '../NavBar'
+
+class EmojiGame extends Component {
+  render() {
+    return <NavBar />
+  }
+}
+
+export default EmojiGame
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,5 @@
+// Write your code here.
+
+const NavBar = () => <h1>NavBar Component</h1>
+
+export default NavBar
