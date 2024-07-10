e65370ffcbcaeabe2218dc5d4e64126bee285c8d
Bilal <bilalmohammed261@gmail.com>
2024-07-10 19:15:45 +0000

    coded for Navbar component

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,5 +1,16 @@
 // Write your code here.
+import './index.css'
 
-const NavBar = () => <h1>NavBar Component</h1>
+const NavBar = () => (
+  <div className="navbar-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
+      alt="emoji logo"
+    />
+    <p>Emoji Game</p>
+    <p>Score:0</p>
+    <p>Top Score:0</p>
+  </div>
+)
 
 export default NavBar
