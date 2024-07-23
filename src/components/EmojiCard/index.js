f0c8d414cefa640ad44d11fe7f9fffdbb1ba8756
Bilal <bilalmohammed261@gmail.com>
2024-07-23 16:54:13 +0000

    coded for Emoji Game and Emoji Card comonents

--- src/components/EmojiCard/index.js
+++ src/components/EmojiCard/index.js
@@ -1,1 +1,19 @@
 // Write your code here.
+import './index.css'
+
+const EmojiCard = props => {
+  const {emojiDetails, shuffleEmojis} = props
+  const {emojiUrl, emojiName} = emojiDetails
+  const onClickEmoji = () => {
+    shuffleEmojis()
+  }
+  return (
+    <li className="card-container">
+      <button type="button" onClick={onClickEmoji}>
+        <img src={emojiUrl} alt={emojiName} />
+      </button>
+    </li>
+  )
+}
+
+export default EmojiCard
--- src/components/EmojiGame/index.js
+++ src/components/EmojiGame/index.js
@@ -15,9 +15,42 @@ import {Component} from 'react'
 
 import NavBar from '../NavBar'
 
+import EmojiCard from '../EmojiCard'
+
+import './index.css'
+
 class EmojiGame extends Component {
+  constructor(props) {
+    super(props)
+    const {emojisList} = props
+    this.state = {
+      emojisList,
+    }
+  }
+
+  shuffleEmojis = () => {
+    const {emojisList} = this.state
+    const shuffledEmojisList = emojisList.sort(() => Math.random() - 0.5)
+    this.setState({emojisList: shuffledEmojisList})
+  }
+
   render() {
-    return <NavBar />
+    const {emojisList} = this.state
+    return (
+      <div className="app-container">
+        <NavBar />
+
+        <ul className="emojis-container">
+          {emojisList.map(eachEmoji => (
+            <EmojiCard
+              key={eachEmoji.id}
+              emojiDetails={eachEmoji}
+              shuffleEmojis={this.shuffleEmojis}
+            />
+          ))}
+        </ul>
+      </div>
+    )
   }
 }
 
