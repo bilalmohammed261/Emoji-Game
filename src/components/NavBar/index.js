471091e0d3ab3cd5d890d68501ada72535350582
Bilal <bilalmohammed261@gmail.com>
2024-07-26 17:01:59 +0000

    made chabes to NavBar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,16 +1,28 @@
 // Write your code here.
+import {Component} from 'react'
+
 import './index.css'
 
-const NavBar = () => (
-  <div className="navbar-container">
-    <img
-      src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
-      alt="emoji logo"
-    />
-    <p>Emoji Game</p>
-    <p>Score:0</p>
-    <p>Top Score:0</p>
-  </div>
-)
+class NavBar extends Component {
+  state = {
+    score: 0,
+    topScore: 0,
+  }
+
+  render() {
+    const {score, topScore} = this.state
+    return (
+      <div className="navbar-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
+          alt="emoji logo"
+        />
+        <p>Emoji Game</p>
+        <p>Score:{score}</p>
+        <p>Top Score:{topScore}</p>
+      </div>
+    )
+  }
+}
 
 export default NavBar
